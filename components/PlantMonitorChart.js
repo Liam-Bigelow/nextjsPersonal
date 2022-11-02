2c70c21381dd488cbd67aa35ec88f37aec37d970
Liam-Bigelow <liamdbigelow@gmail.com>
2022-11-02 10:12:26 -0600

    Chore: need to have scales for animation

--- components/PlantMonitorChart.js
+++ components/PlantMonitorChart.js
@@ -74,12 +74,53 @@ export default function PlantMonitorChart({plantKey}) {
             })
     }, []);
 
+    // setup animation for option
+    const totalDuration = 10000;
+    const delayBetweenPoints = 1000;
+    const previousY = (ctx) => {
+        console.log( ctx );
+        ctx.index === 0 ? ctx.chart.scales.y.getPixelForValue(100) : ctx.chart.getDatasetMeta(ctx.datasetIndex).data[ctx.index - 1].getProps(['y'], true).y
+    };
+
     const [options, setOptions] = useState({
         responsive: true,
+        animation: {
+            x: {
+              type: 'number',
+              easing: 'linear',
+              duration: delayBetweenPoints,
+              from: NaN, // the point is initially skipped
+              delay(ctx) {
+                if (ctx.type !== 'data' || ctx.xStarted) {
+                  return 0;
+                }
+                ctx.xStarted = true;
+                return ctx.index * delayBetweenPoints;
+              }
+            },
+            y: {
+              type: 'number',
+              easing: 'linear',
+              duration: delayBetweenPoints,
+              from: previousY,
+              delay(ctx) {
+                if (ctx.type !== 'data' || ctx.yStarted) {
+                  return 0;
+                }
+                ctx.yStarted = true;
+                return ctx.index * delayBetweenPoints;
+              }
+            }
+        },
         plugins: {
             legend: {
                 position: 'bottom',
             },
+            scales: {
+                x: {
+                  type: 'linear'
+                }
+              }
         },
     });
     
