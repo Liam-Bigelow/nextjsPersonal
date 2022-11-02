52d5b7a47fe10ba6db5580e869493779606f7248
Liam-Bigelow <liamdbigelow@gmail.com>
2022-11-01 21:00:46 -0600

    Feat: now quering db

--- netlify/functions/getPlantData.js
+++ netlify/functions/getPlantData.js
@@ -1,52 +1,22 @@
 
-var co = require('co');
-var mongoose = require('mongoose');
 
-let conn = null;
 
-const uri = "mongodb+srv://sensorHub:T4fOetgmACUOjcMi@cluster0.kewxbka.mongodb.net/sensorData?retryWrites=true&w=majority";
+const { MongoClient } = require("mongodb");
 
-export const handler = (event, context, callback) => {
+const mongoClient = new MongoClient(process.env.MONGODB_URI);
 
-    context.callbackWaitsForEmptyEventLoop = false;
+const clientPromise = mongoClient.connect();
 
-    run()
-        .then(res => {
-            callback(null, res);
-        })
-        .catch(error => callback(error));
-};
-
-function run() {
-    return co(function*() {
-        return new Promise( async (resolve, reject) => {
-            if (conn == null) {
-                conn = mongoose.createConnection(uri, {
-                    bufferCommands: false,
-                    bufferMaxEntries: 0
-                });
-                
-                conn.model('reads', new mongoose.Schema({
-                    moisture: Number,
-                    humidity: Number,
-                    temperature: Number,
-                    plant: String,
-                    date: {
-                        type: Date,
-                        default: Date.now,
-                        required: true
-                    }
-                }));
-            }
-    
-            const M = conn.model('reads');
-            const doc = await M.find();
-    
-            const response = {
-                statusCode: 200,
-                body: JSON.stringify(doc)
-            };
-            resolve( response );
-        });
-    });
+export const handler = async (event) => {
+    try {
+        const database = (await clientPromise).db(process.env.MONGODB_DATABASE);
+        const collection = database.collection(process.env.MONGODB_COLLECTION);
+        const results = await collection.find({}).limit(10).toArray();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(results),
+        }
+    } catch (error) {
+        return { statusCode: 500, body: error.toString() }
+    }
 }
