e043d688fb23916414afd095bc5bef4485ae4050
Liam-Bigelow <liamdbigelow@gmail.com>
2022-11-21 18:29:12 -0600

    Feat: about us now resizing

--- components/AboutPage.js
+++ components/AboutPage.js
@@ -11,9 +11,8 @@ export default function AboutPage() {
         <div className={styles["about-page"]}>
             <div className={styles["about-container"]}>
                 <section className="row" style={{margin: "15px"}} >
-                    <img className={styles["about-image"]} src={"./me.webp"} alt="me" />
-
-                    <div className={styles["about-text"]}>
+                    <img className={`${styles["about-image"]} col s8 m4 l3`} src={"./me.webp"} alt="me" />
+                    <div className={`${styles["about-text"]} col s12 m8 l9`} >
                         Hey,<br/>
                         <span style={{color: "#FBA01D", fontWeight: "600"}}>I'm Liam.</span> A Full-Stack Software Developer based in Saskatoon, Saskatchewan.<br/>
                         <br/>
@@ -22,8 +21,8 @@ export default function AboutPage() {
                         When I'm not writing code I enjoy 🏂 snowboarding and 🌳 wood-working!
                     </div>
 
-                    <div className={styles["socials"]}>
-                        <a href="https://www.linkedin.com/">
+                    <div className={`${styles["socials"]} col s12`}>
+                        <a href="www.linkedin.com/in/liam-bigelow">
                             <FaLinkedin />
                         </a>
                         <a href="https://github.com/Liam-Bigelow">
