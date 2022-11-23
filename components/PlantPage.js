c1212c8831d6d2c085c88bd3ac26480338c5111a
Liam-Bigelow <liamdbigelow@gmail.com>
2022-11-22 20:34:05 -0600

    style: added border to img

--- components/PlantPage.js
+++ components/PlantPage.js
@@ -26,7 +26,9 @@ export default function PlantPage() {
 
             <h3 style={{fontWeight: 700}}><span>v1.0</span> - First Attempt 🎍</h3>
             <div className={`${styles["plant-v1"]} row`}>
-                <img src="/plantMonitorV1.webp" className="col s12 m6" />
+                <div className="col s12 m6">
+                    <img src="/plantMonitorV1.webp" style={{border: "10px solid #fcb649", width: "100%"}} />
+                </div>
                 <div className="col s12 m6">
                     <p>Here is my first attempt at building an autimated plant waterer. It consisted of a Arduino Uno, Moisture sensor, Accelerometer, 3 Mini Pumps, an LCD Display, a Relay, a water level sensor, and finally alot of cables.</p>
                     <p>What I learned from this build was that it is very hard to get an accurate read of the moisture level of a pot of soil without having a very long moisture sensor. I also learned that using a rule for activating the pumps such as</p>
